868de89c19d57d9362706739b6cee98f437f16a0
Rui Lopes <rgl@ruilopes.com>
2021-09-30 20:24:33 +0100

    install docker

---
+++ provision-docker.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+set -euxo pipefail
+
+# NB execute apt-cache madison docker-ce to known the available versions.
+docker_version="${1:-20.10.8}"; shift || true
+
+# prevent apt-get et al from asking questions.
+# NB even with this, you'll still get some warnings that you can ignore:
+#     dpkg-preconfigure: unable to re-open stdin: No such file or directory
+export DEBIAN_FRONTEND=noninteractive
+
+# install docker.
+# see https://docs.docker.com/engine/install/ubuntu/
+apt-get install -y apt-transport-https software-properties-common
+wget -qO- https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+docker_apt_version="$(apt-cache madison docker-ce | awk "/$docker_version~/{print \$3}")"
+apt-get install -y "docker-ce=$docker_apt_version" "docker-ce-cli=$docker_apt_version" containerd.io
+
+# configure it.
+systemctl stop docker
+cat >/etc/docker/daemon.json <<EOF
+{
+    "experimental": false,
+    "debug": false,
+    "features": {
+        "buildkit": true
+    },
+    "log-driver": "journald",
+    "labels": [
+        "os=linux"
+    ],
+    "hosts": [
+        "fd://"
+    ],
+    "containerd": "/run/containerd/containerd.sock"
+}
+EOF
+# start docker without any command line flags as its entirely configured from daemon.json.
+install -d /etc/systemd/system/docker.service.d
+cat >/etc/systemd/system/docker.service.d/override.conf <<'EOF'
+[Service]
+ExecStart=
+ExecStart=/usr/bin/dockerd
+EOF
+systemctl daemon-reload
+systemctl start docker
+
+# let the ubuntu user manage docker.
+usermod -aG docker ubuntu
