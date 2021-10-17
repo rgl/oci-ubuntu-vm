80bc0f6d12e53f4e8124d44cc7bc68280f0fb230
Rui Lopes <rgl@ruilopes.com>
2021-10-17 11:58:22 +0100

    configure the shell

--- provision-base.sh
+++ provision-base.sh
@@ -14,3 +14,37 @@ set ruler
 set laststatus=2
 set nobackup
 EOF
+
+
+#
+# configure the shell.
+
+cat >/etc/profile.d/login.sh <<'EOF'
+[[ "$-" != *i* ]] && return # bail when not running interactively.
+export EDITOR=vim
+export PAGER=less
+alias l='ls -lF --color'
+alias ll='l -a'
+alias h='history 25'
+alias j='jobs -l'
+EOF
+
+cat >/etc/inputrc <<'EOF'
+set input-meta on
+set output-meta on
+set show-all-if-ambiguous on
+set completion-ignore-case on
+"\e[A": history-search-backward
+"\e[B": history-search-forward
+"\eOD": backward-word
+"\eOC": forward-word
+EOF
+
+# configure the ubuntu user home.
+su ubuntu -c bash <<'EOF'
+set -euxo pipefail
+
+cat >~/.bash_history <<'EOFU'
+sudo -i
+EOFU
+EOF
