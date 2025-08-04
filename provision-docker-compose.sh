ce3ad10baeb0a4a13fc2faf7ef5db5705b6385c1
Rui Lopes <rgl@ruilopes.com>
2025-08-04 23:27:48 +0000

    upgrade to docker compose 2.39.1

--- provision-docker-compose.sh
+++ provision-docker-compose.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 set -euxo pipefail
 
-docker_compose_version="${1:-v2.15.1}"; shift || true
-
 # download.
+# see https://docs.docker.com/compose/install/#install-compose-on-linux-systems
 # see https://github.com/docker/compose/releases
-# see https://docs.docker.com/compose/cli-command/#install-on-linux
-docker_compose_url="https://github.com/docker/compose/releases/download/$docker_compose_version/docker-compose-linux-$(uname -m)"
+# renovate: datasource=github-releases depName=docker/compose
+docker_compose_version='2.39.1'
+docker_compose_url="https://github.com/docker/compose/releases/download/v$docker_compose_version/docker-compose-linux-$(uname -m)"
 wget -qO /tmp/docker-compose "$docker_compose_url"
 
 # install.
-install -d ~/.docker/cli-plugins
-install -m 555 /tmp/docker-compose ~/.docker/cli-plugins
+install -d /usr/local/lib/docker/cli-plugins
+install -m 555 /tmp/docker-compose /usr/local/lib/docker/cli-plugins
 rm /tmp/docker-compose
 docker compose version
