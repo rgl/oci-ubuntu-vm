06028a08b3e453dc082210f900b5177dd131b5ed
Rui Lopes <rgl@ruilopes.com>
2025-08-04 23:27:48 +0000

    upgrade to docker 28.3.3

--- provision-docker.sh
+++ provision-docker.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 set -euxo pipefail
 
-# NB execute apt-cache madison docker-ce to known the available versions.
-docker_version="${1:-20.10.23}"; shift || true
+# see https://github.com/moby/moby/releases
+# renovate: datasource=github-releases depName=moby/moby
+docker_version='28.3.3'
 
 # prevent apt-get et al from asking questions.
 # NB even with this, you'll still get some warnings that you can ignore:
@@ -10,12 +11,14 @@ docker_version="${1:-20.10.23}"; shift || true
 export DEBIAN_FRONTEND=noninteractive
 
 # install docker.
-# see https://docs.docker.com/engine/install/ubuntu/
+# see https://docs.docker.com/engine/installation/linux/docker-ce/ubuntu/#install-using-the-repository
 apt-get install -y apt-transport-https software-properties-common
-wget -qO- https://download.docker.com/linux/ubuntu/gpg | apt-key add -
-add-apt-repository "deb [arch=$(dpkg --print-architecture)] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-docker_apt_version="$(apt-cache madison docker-ce | awk "/$docker_version~/{print \$3}")"
-apt-get install -y "docker-ce=$docker_apt_version" "docker-ce-cli=$docker_apt_version" containerd.io
+wget -qO- https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/apt/keyrings/download.docker.com.gpg
+echo "deb [arch=amd64 signed-by=/etc/apt/keyrings/download.docker.com.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" >/etc/apt/sources.list.d/docker.list
+apt-get update
+apt-cache madison docker-ce
+docker_package_version="$(apt-cache madison docker-ce | awk "/$docker_version/{print \$3}")"
+apt-get install -y "docker-ce=$docker_package_version" "docker-ce-cli=$docker_package_version" containerd.io
 
 # configure it.
 systemctl stop docker
